e9a0c1782c8aa4355371866393518c7ff3c4f2a8
Frija-farah <farah.131.frija@gmail.com>
2023-03-20 11:07:27 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+  * main - the main function
+  *
+  * Return: always 0
+  */
+int main(void)
+{puts("with proper grammar, but the outcome is a piece of art,");
+        return (0); }
